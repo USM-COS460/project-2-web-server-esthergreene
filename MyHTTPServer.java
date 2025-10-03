eae11f834fe85a8489f6497e57691d623f5c6697
Esther <160644164+esthergreene@users.noreply.github.com>
2025-10-03 11:01:54 -0400

    adding MyHTTPServer to the project

---
+++ MyHTTPServer.java
@@ -0,0 +1,7 @@
+/**
+ * Name: MyHTTPServer
+ * Author: esthergreene
+ * Project: Program 2
+ * Version: 10/03/2025
+ * Note: Sources have been cited and appear above respective code (e.g., "References")
+ */
