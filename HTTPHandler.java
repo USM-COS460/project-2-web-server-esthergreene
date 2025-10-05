28128c1b96acaf1f14a2b255727efb2382196c37
Esther <160644164+esthergreene@users.noreply.github.com>
2025-10-05 17:51:21 -0400

    intro notes for HTTPHandler

---
+++ HTTPHandler.java
@@ -0,0 +1,11 @@
+/**
+ * Name: HTTPHandler
+ * Author: esthergreene
+ * Project: Program 2
+ * Version: 10/05/2025
+ * Use: Used in tandem with MyHTTPServer class. HTTPHandler reads the request from a browser and
+ *      figures out what file the browser is asking fro inside the document root. It then sends a proper HTTP
+ *      response back to the browser such as status line, required headers, and actual file contents (HTML, CSS, etc.)
+ *      HTTPHandler then closes the connection when done (eventually).
+ * Note: Sources have been cited and appear above respective code (e.g., "References:")
+ */
