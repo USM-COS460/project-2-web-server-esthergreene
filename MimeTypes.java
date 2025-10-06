f21fc4a318af0d4c75804ca870fef27a3303b36c
Esther <160644164+esthergreene@users.noreply.github.com>
2025-10-05 22:22:42 -0400

    class title of MimeTypes (MT)

---
+++ MimeTypes.java
@@ -0,0 +1,8 @@
+/**
+ * Name: MimeTypes
+ * Author: esthergreene
+ * Project: Program 2
+ * Version: 10/05/2025
+ * Use: 
+ * Note: Sources have been cited and appear above respective code (e.g., "References:")
+ */
