b54b213de3c6889167a32e35c94d48d2cdac7b69
Esther <160644164+esthergreene@users.noreply.github.com>
2025-10-17 13:09:53 -0400

    MimeTypes start

--- MimeTypes.java
+++ MimeTypes.java
@@ -3,6 +3,30 @@
  * Author: esthergreene
  * Project: Program 2
  * Version: 10/05/2025
- * Use: 
+ * Use: A simple lookup helper that matches file extensions to their correct content-type values
+ *      so browsers know what kind of file they are receiving. Keeps a list of file extensions and 
+ *      their matching MIME types. 
  * Note: Sources have been cited and appear above respective code (e.g., "References:")
  */
+
+ /**
+  * Resources:
+  * https://www.w3schools.com/java/java_hashmap.asp
+  * https://www.geeksforgeeks.org/java/map-interface-in-java/
+  */
+ import java.util.HashMap;
+ import java.util.Map;
+
+ /**
+  * Resources:
+  * https://www.w3schools.com/java/java_hashmap.asp
+  *
+  *
+  */
+ public class MimeTypes  {
+    private static final Map<String, String> types = new HashMap<>();
+
+    static {
+        
+    }
+ }
